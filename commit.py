b46e33d3f74914e9f13a6deb7098e2167d380b33
Dinithi Gunawardena <95086373+DinithiG@users.noreply.github.com>
2023-10-05 23:02:20 +0530

    Create commit.py

---
+++ commit.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import davies_bouldin_score, calinski_harabasz_score
+from sklearn.impute import SimpleImputer
